dbddec73d8e4441ee8f0d0fef0a9005ef417bdc2
Derek Lewis <7467911+derekelewis@users.noreply.github.com>
2025-08-10 19:01:32 -0500

    change MAX_TOKENS

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ openai = OpenAI()
 # Parameters
 MAX_STORIES = 5  # Number of stories to process
 MAX_CONTENT_LENGTH = 3000  # Max length of story content to summarize
-MAX_TOKENS = 2000
+MAX_TOKENS = None
 TEMPERATURE = 1.0
 SYSTEM_PROMPT = """You are an assistant that summarizes news articles.
 Please ensure that every article is summarized accurately. Insert the provided
